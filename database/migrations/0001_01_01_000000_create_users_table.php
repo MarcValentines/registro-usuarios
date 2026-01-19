b6eec34e612f88d0108866807601ac45f664f08d
Marc <marcvalentinesmiro@gmail.com>
2026-01-19 19:29:40 +0100

    Modificación base de la tabla de usuarios
    Se añade el rol, el nombre de usuario y el apellido a la tabla de los usuarios

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('username')->unique(); //nombre de user
             $table->string('name');
+            $table->string('surename'); //apellido
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('rol', ['admin', 'usuario'])->default('usuario'); //añadir los roles de usuarios
             $table->rememberToken();
             $table->timestamps();
         });
